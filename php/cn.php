c7f7ef6398659af09229e642c8c612be48cfb7b1
= <=>
2020-07-17 10:53:54 -0500

    Se realiza el login y se modifican los archivos necesarios

--- php/cn.php
+++ php/cn.php
@@ -1,9 +1,22 @@
 <?php 
-    $conexion = mysqli_connect("localhost", "root", "", "luxury");
-    /*if(!$conexion){
-        echo "Error al conectarnos a la base de datos <br ?>";
-    }
-    else{
+    define("SERVIDOR","localhost");
+    define("USUARIO","root");
+    define("PASS","");
+    define("DB","luxury");
+    
+    function conexionSQL(){
+        $link = new mysqli(SERVIDOR,USUARIO,PASS,DB);
+        if($link->connect_error) {
+            $error = "Error de conexion: ".$link->connect_errno
+                    ."Mensaje: ".$link->connect_error;
+            die($error);
+        }else{
+            $q = "SET CHARACTER SET UTF8";
+            $link->query($q);
+            return $link;
+    /*else{
         echo "Conectado a la base de datos <br ?>"; 
     }*/
+        }
+    }
 ?>
