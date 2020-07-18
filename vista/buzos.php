f1a2c9ad56753fe6678aceb75d9e8095ccbea6f2
= <=>
2020-07-17 20:31:40 -0500

    Se arreglan links de las vistas

--- vista/buzos.php
+++ vista/buzos.php
@@ -25,7 +25,7 @@
 <body >
     <header>
         <nav class="navbar navbar-expand-md navbar-light" style="background-color: #e3f2fd;">
-        <a href="index.php"><img class="not" src="../imagenes/logo_luxury.jpg" width="80px" style="border-radius: 150px"></a> 
+        <a href="../php/index.php"><img class="not" src="../imagenes/logo_luxury.jpg" width="80px" style="border-radius: 150px"></a> 
           <a class="navbar-brand py-0 ml-3 mr-5" href="../php/index.php">LUXURY STORE</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
